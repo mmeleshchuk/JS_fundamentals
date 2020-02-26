ad883b15dec5ea3096cea278ce2adedd0bfbaab5
mmeleshchuk <61464784+mmeleshchuk@users.noreply.github.com>
2020-02-26 16:11:13 +0200

    Add a folder HW1 with solutions of homework1
    Folder contains files index.html, homework1.js

---
+++ HW1/homework1.js
@@ -0,0 +1,23 @@
+/* //Page 1, Task2
+alert('Mykhailo')
+
+//Page 1, Task 3
+let exp1 = 15;
+alert(exp1);
+exp1 = exp1 + 5;
+alert(exp1);
+
+//Page 1, Task 4
+let study1 = 'HTML + CSS', study2 = 'JS';
+alert(study1);
+alert(study2);
+study1 = study2;
+alert(study1);
+alert(study2);
+
+//Page 1, Task 5
+let userName = prompt('Hello! Enter your name:', '');
+isNaN(userName) && userName !== null && userName !== '' ? alert(`Welcome to our web-portal, ${userName}`) : alert(`Welcome to our web-portal, stranger`);
+*/
+
+//Page 2, Task 1
